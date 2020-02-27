2d162f423579fcf691e05c1ffdbb6ed3d491000b
buckn <nathanieljbuck7@gmail.com>
2020-02-27 13:19:17 -0500

    started gui

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,82 @@
-use iced::{button, Button, Column, Element, Sandbox, Settings, Text, Length, HorizontalAlignment, Image, Container, scrollable, Space};
+//#![windows_subsystem = "windows"]
 
-pub fn main() {
-    Counter::run(Settings::default())
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+extern crate web_view;
+
+use web_view::*;
+
+fn main() {
+    let html = format!(
+        r#"
+{doctype}
+<html>
+    <head>
+        {head}
+        {styles}
+        {scripts}
+    </head>
+    <body>
+        {body}
+    </body>
+</html>"#,
+        doctype = include_str!("html/doctype.html"),
+        head = include_str!("html/head.html"),
+        body = include_str!("html/body.html"),
+        styles = inline_style(include_str!("css/index.css")),
+        scripts = inline_script(include_str!("js/index.js")),
+        );
+
+    println!("{}", html);
+
+    let mut webview = web_view::builder()
+    .title("sens")
+    .content(Content::Html(html))
+    .size(500, 500)
+    .resizable(true)
+    .debug(true)
+    .user_data(())
+    .invoke_handler(|_webview, _arg| Ok(()))
+    .build()
+    .unwrap();
+
+    webview.set_color((156, 39, 176));
+
+    let res = webview.run().unwrap();
+
+    println!("final state: {:?}", res);
 }
 
-#[derive(Default)]
-struct Counter {
-    value: i32,
-    increment_button: button::State,
-    decrement_button: button::State,
-    container: scrollable::State,
+fn render(webview: &mut WebView<Vec<Task>>) -> WVResult {
+    let render_tasks = {
+        let tasks = webview.user_data();
+        println!("{:#?}", tasks);
+        format!("rpc.render({})", serde_json::to_string(tasks).unwrap())
+    };
+    webview.eval(&render_tasks)
 }
 
-#[derive(Debug, Clone, Copy)]
-enum Message {
-    IncrementPressed,
-    DecrementPressed,
+#[derive(Debug, Serialize, Deserialize)]
+struct Task {
+    name: String,
+    done: bool,
 }
 
-impl Sandbox for Counter {
-    type Message = Message;
+#[derive(Deserialize)]
+#[serde(tag = "cmd", rename_all = "camelCase")]
+pub enum Cmd {
+    Init,
+    Log { text: String },
+    AddTask { name: String },
+    MarkTask { index: usize, done: bool },
+    ClearDoneTasks,
+}
 
-    fn new() -> Self {
-        Self::default()
-    }
+fn inline_style(s: &str) -> String {
+    format!(r#"<style type="text/css">{}</style>"#, s)
+}
 
-    fn title(&self) -> String {
-        String::from("A simple counter")
-    }
-
-    fn update(&mut self, message: Message) {
-        match message {
-            Message::IncrementPressed => {
-                self.value += 1;
-            }
-            Message::DecrementPressed => {
-                self.value -= 1;
-            }
-        }
-    }
-
-    fn view(&mut self) -> Element<Message> {
-            Column::new()
-            .push(Column::new()
-            .padding(20)
-            .push(
-                Button::new(&mut self.increment_button, Text::new("Increment"))
-                    .on_press(Message::IncrementPressed),
-            )
-            .push(Text::new(self.value.to_string()).size(50))
-            .push(
-                Button::new(&mut self.decrement_button, Text::new("Decrement"))
-                    .on_press(Message::DecrementPressed),
-            )
-            .into())
-            .push(Text::new(
-                "Iced supports scrollable content. Try it out! Find the \
-                 button further below.",
-            ))
-            .push(
-                Text::new(
-                    "Tip: You can use the scrollbar to scroll down faster!",
-                )
-                .size(16),
-            )
-            .push(Column::new().height(Length::Units(4096)))
-            .push(
-                Text::new("You are halfway there!")
-                    .size(30)
-                    .horizontal_alignment(HorizontalAlignment::Center),
-            )
-            .push(Column::new().height(Length::Units(4096)))
-            .push(ferris(300))
-            .push(
-                Text::new("You made it!")
-                    .size(50)
-                    .horizontal_alignment(HorizontalAlignment::Center),
-            )
-    }
+fn inline_script(s: &str) -> String {
+    format!(r#"<script type="text/javascript">{}</script>"#, s)
 }
